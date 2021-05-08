aebab96f5fa921e4f5d54fcf10eb80973dfc63e5
danielrandy <danielrandy5@gmail.com>
2021-05-08 18:01:27 -0400

    Day 1

--- Day1.py
+++ Day1.py
@@ -1,12 +1,17 @@
+#Day 1: setting up python, python on cli #!/bin/env phython 3.7
+#completion Date: May 7th 2021
 
 print (" randy" + str(4) )
 print(" this\n\t is \n ot")
+
 print(22 // 8) #remender
 print (2 ** 3) #exponent
+
 #print( "you can add a string with a number:" + 42) Unless you add str()
 print("you can add a string to a number: " + str(42))
 print("this is also {}  and + {}".format("zizi","sisi"))
-print("this is good {} \n\t but will only {} work here" .format("okay","didi5"))
+print("this is good {} \n\t but will only {} work here" .format("okay","didi5")) #tab with formating
 ## fomarting with {} {} .format("1st","2") will work. you cam also use
 
 print(" this will work if i make this ")
+print("{1} {0}".format(3,4)) # this will call in an array form
