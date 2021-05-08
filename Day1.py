966aec1fa7fd6d241c0d74542435a293587c624c
danielrandy <danielrandy5@gmail.com>
2021-05-08 17:52:14 -0400

    Day 1

---
+++ Day1.py
@@ -0,0 +1,12 @@
+
+print (" randy" + str(4) )
+print(" this\n\t is \n ot")
+print(22 // 8) #remender
+print (2 ** 3) #exponent
+#print( "you can add a string with a number:" + 42) Unless you add str()
+print("you can add a string to a number: " + str(42))
+print("this is also {}  and + {}".format("zizi","sisi"))
+print("this is good {} \n\t but will only {} work here" .format("okay","didi5"))
+## fomarting with {} {} .format("1st","2") will work. you cam also use
+
+print(" this will work if i make this ")
