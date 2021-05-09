5f376afd4374dc99f8be46939460fd6439e90781
danielrandy <danielrandy5@gmail.com>
2021-05-08 22:11:21 -0400

    Day2

---
+++ Day 2.py
@@ -0,0 +1,9 @@
+#Day 2: String formatting
+#completion Date:
+
+cheers = "where everybody knows YOUR  name "
+print(cheers.capitalize()) #Capitalizes what need to be capitalzed
+print(cheers.title()) #Capitalizes the first leter of a word
+print(cheers.lower()) #changes everything to a lowercase
+print(cheers.upper()) #changes every word to upercase
+print("this CAN also Work".upper()) #this can also work with everything
